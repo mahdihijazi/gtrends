6349a5b9a8fd1e9f1c56b0958af37dac493f7ee1
Mahdi Hijazi <mahdi.hijazi@booking.com>
2021-01-04 20:17:50 +0100

    support query trends for multiple countries at once

---
+++ Country.py
@@ -0,0 +1,11 @@
+class Country:
+    def __init__(self, name, iso2):
+        self.name = name
+        self.iso2 = iso2
+
+
+saudi_arabia = Country("Saudi Arabia", "SA")
+jordan = Country("Jordan", "JO")
+egypt = Country("Egype", "EG")
+
+arabic_countries = [saudi_arabia, jordan, egypt]
--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy
 import csv
+from Country import *
+
 
 def write_csv_file(file_name, array):
-    with open(file_name,"w+") as my_csv:
-        csvWriter = csv.writer(my_csv,delimiter=',')
+    with open(file_name, "w+") as my_csv:
+        csvWriter = csv.writer(my_csv, delimiter=',')
         csvWriter.writerows(array)
 
 
@@ -16,22 +18,43 @@ def get_search_interest_over_time(keyword_list, country_iso2, timeframe='today 5
     array = res.to_numpy()
 
     # sum all scores for the past 5 years
-    sum = array.sum(axis=0)[0]
+
+    sum = array.sum(axis=0)[0] if (array.size > 0) else 0
     # count how many score points we have
     scoresCount = array.shape[0]
 
-    avg = sum / scoresCount
-    print(avg)
-
-    list = [["Term", "Saudi Arabia"]]
-    list.append(["good habits", "60"])
-
-    write_csv_file("output.csv", list)
+    avg = sum / scoresCount if (scoresCount != 0) else 0
+    return avg
 
 
-kw_list = ["good habits"]
-get_search_interest_over_time(kw_list, 'SA')
+topics_list = ["good habits", "StrengthsFinder", "Getting Things Done", "برمجه"]
 
 
+# 1 raw for the header 7 one row for the total
+lastRow = len(arabic_countries) + 2
 
+# set first column title
+out = [["Country"]]
+for topic in topics_list:
+    # append the second column in the sheet (Topic)
+    out[0].append(topic)
+    countries_rows = 1
+    # sum of all countries average scores
+    score_sum = 0
+    for country in arabic_countries:
+        result = get_search_interest_over_time([topic], country.iso2)
+        score_sum = score_sum + result
 
+        if countries_rows < len(out):
+            out[countries_rows].append(result)
+        else:
+            out.append([country.name, result])
+        countries_rows = countries_rows + 1
+
+    # append the last row in the sheet which is the total of all countries averages
+    if len(out) < lastRow:
+        out.append(["Total", score_sum])
+    else:
+        out[lastRow - 1].append(score_sum)
+
+write_csv_file("output.csv", out)
